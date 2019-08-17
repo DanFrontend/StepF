0db76b37c489d9590bfde1cb7ee1f76b2f048c4c
Ovcherenko Anton <ovcherenko.a@gmail.com>
2019-08-17 15:00:04 +0300

    creating revolutionary editor block

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 'use strict';
- 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-
+const browserSync = require('browser-sync');
+const autoprefixer   = require('gulp-autoprefixer');
 sass.compiler = require('node-sass');
- 
-gulp.task('sass-01', function () {
-    return gulp.src('src/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/style'));
+gulp.task('scss', function () {
+   return gulp.src('src/scss/*.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(autoprefixer({
+       browsers: ['last 10 versions'],
+       cascade: false
+   }))
+   .pipe(gulp.dest('dist/style'))
+   .pipe(browserSync.reload({stream: true}));
 });
- 
-gulp.task('watch', function () {
-    gulp.watch('src/scss/*.scss', gulp.series('sass-01'));
+gulp.task('browser-sync', function(done) {
+   browserSync.init({
+       server: {
+           baseDir: './'
+       },
+       notify: false
+   });
+   done();
 });
-
-
+gulp.task('dev', gulp.parallel('browser-sync',  function () {
+   gulp.watch('src/scss/*.scss', gulp.series('scss'))
+}))
